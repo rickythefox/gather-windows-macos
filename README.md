b7052c1cd96660c759955be54c38f7f1f3f876e0
Richard Ginzburg <richard@ginzburg.se>
2020-11-11 23:14:49 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gather-windows-macos
+
+When connecting and disconnecting your MacBook to an external display your windows sometimes end on the laptop display. This tool will get all your windows to the external display (that is set as primary).
+
+## How it works
+The tool uses the fact that when you toggle display mirroring on and off MacOS gathers all your windows on the primary display.
+
+## How to build
+```
+mkdir build
+cmake -S . -B build/
+cmake --build build --target all -- -j 12
+```
+
+## How to get binaries
+You can download the binary from the Releases tab.
---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <ApplicationServices/ApplicationServices.h>
+
+void GetDisplays(CGDirectDisplayID* mainDisplayId, CGDirectDisplayID* secondaryDisplayId) {
+    CGDirectDisplayID t = CGMainDisplayID();
+    *mainDisplayId = CGMainDisplayID();
+    CGDirectDisplayID onlineDisplays[10];
+    uint32_t nOnlineDisplays;
+
+    CGDisplayErr err = CGGetOnlineDisplayList(10, onlineDisplays, &nOnlineDisplays);
+    if (err != 0) {
+        printf("Error getting online displays: %d\n", err);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < nOnlineDisplays; i++) {
+        if (onlineDisplays[i] != *mainDisplayId) {
+            *secondaryDisplayId = onlineDisplays[i];
+            break;
+        }
+    }
+}
+
+void ToggleMirroring(bool on, CGDirectDisplayID mainDisplayId, CGDirectDisplayID secondaryDisplayId) {
+    // Begin configuration
+    CGDisplayConfigRef configRef;
+    CGError err = CGBeginDisplayConfiguration(&configRef);
+    if (err != 0) {
+        printf("Error initiating display configuration: %d\n", err);
+        exit(EXIT_FAILURE);
+    }
+
+    // Mirror displays or cancel mirroring
+    if (!on)
+        mainDisplayId = kCGNullDirectDisplay;
+    err = CGConfigureDisplayMirrorOfDisplay(configRef, secondaryDisplayId, mainDisplayId);
+    if (err != 0) {
+        printf("Error configuring displays: %d\n", err);
+        exit(EXIT_FAILURE);
+    }
+
+    // Apply the changes
+    err = CGCompleteDisplayConfiguration(configRef, kCGConfigurePermanently);
+    if (err != 0) {
+        printf("Error applying configuration: %d\n", err);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    printf("TEST");
+    CGDirectDisplayID mainDisplayId, secondaryDisplayId;
+    GetDisplays(&mainDisplayId, &secondaryDisplayId);
+
+    bool alreadyMirrored = CGDisplayIsInMirrorSet(mainDisplayId);
+    if (alreadyMirrored) {
+        printf("Displays mirrored, will not toggle\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    if (mainDisplayId <= 0 || secondaryDisplayId <= 0) {
+        printf("Can't find a main and a secondary display\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ToggleMirroring(true, mainDisplayId, secondaryDisplayId);
+    ToggleMirroring(false, mainDisplayId, secondaryDisplayId);
+
+    return EXIT_SUCCESS;
+}
+
